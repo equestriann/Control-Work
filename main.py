5341edfc91028a9649efbb472dc88a610c60c2e7
equestriann <x.dormostuk@gmail.com>
2023-10-09 22:06:05 +0300

    first func test

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def make_new_array(string):
 
     return array_
 
+strings = [
+    ['hello', '2', 'world', ':-)'],
+    ['1234', '1567', '-2', 'computer science'],
+    ['Russia', 'Denmark', 'Kazan']
+]
+
+for i in strings:
+    print(make_new_array(i))
+
 
 # Вариант №2
 def make_new_array_from_input():
